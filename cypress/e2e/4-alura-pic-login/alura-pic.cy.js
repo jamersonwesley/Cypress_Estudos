ef0e1d12401613edaa7134fe8d4eb6d3aad0163d
Jamersonwesley <jamerson_wesley@hotmail.com>
2022-06-09 09:38:23 -0300

    my third  commit

---
+++ cypress/e2e/4-alura-pic-login/alura-pic.cy.js
@@ -0,0 +1,22 @@
+describe('alura pic login',() =>{
+
+    beforeEach(()=>{
+        cy.visit('https://alura-fotos.herokuapp.com/#/home');
+    }
+    );
+    it.only('login valido', ()=>{
+        cy.login('flavio','123');
+        cy.contains('a','(Logout)').should('be.visible')
+
+    })
+    it.only('login invalido', ()=>{
+
+   cy.login('jamerson','123')
+    cy.on('window:alert' , (str)=>{
+        expect(str).to.equal('Invalid user name or password')
+    })
+
+    })
+
+
+})
