32f21a4ee157af59ff53ee3be8e869c0986117cb
Jamersonwesley <jamerson_wesley@hotmail.com>
2022-06-09 08:31:29 -0300

    My second commit

--- cypress/e2e/3-alura-pic/alurapic.cy.js
+++ cypress/e2e/3-alura-pic/alurapic.cy.js
@@ -19,11 +19,42 @@ describe('login na alura pic',() =>{
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage','Email is required!').should('be.visible');
         cy.contains('button', 'Register').click();
-         cy.contains('ap-vmessage','Full name is required!').should('be.visible');
-          cy.contains('ap-vmessage','User name is required!').should('be.visible');
-           cy.contains('ap-vmessage','Password is required!').should('be.visible');
            cy.get('input[formcontrolname="email"]').type('aaa');
              cy.contains('ap-vmessage','Invalid e-mail').should('be.visible');
+
+
+
+    })
+       it('verifica nome completo',()=>{
+        cy.contains('a','Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+           cy.get('input[formcontrolname="fullName"]').type('a');
+             cy.contains('ap-vmessage','Mininum length is 2').should('be.visible');
+
+
+
+    })
+       it('verifica nome',()=>{
+        cy.contains('a','Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+           cy.get('input[formcontrolname="userName"]').type('a');
+             cy.contains('ap-vmessage','Mininum length is 2').should('be.visible');
+
+
+
+    })
+
+it('verifica senha',()=>{
+        cy.contains('a','Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage','Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+           cy.get('input[formcontrolname="password"]').type('a');
+             cy.contains('ap-vmessage','Mininum length is 8').should('be.visible');
 
 
 
